6187f8ad085aa5c446fe1b62358bb6b1466b2913
abdulmueesp <abdulmueesp5@gmail.com>
2024-05-01 14:47:26 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+     <span className="text-green-500 text-5xl font-bold">Fruit</span>
+    <span className="text-4xl font-light">Shop</span>
+    
+    <div>
+      <span class="text-stone-500 text-4xl">Seoson Specials</span>
+    </div>
+    </>
+   
+  )
+}
+
+export default App
