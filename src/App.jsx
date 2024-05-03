ba08388f587d6d9676dc4759a5b283276a5a2e8d
abdulmueesp <abdulmueesp5@gmail.com>
2024-05-03 19:27:03 +0530

    updated navbar to be responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import Navbar from "./Components/Navbar"
-
+import Navbar from "./Components/Navbar";
 
 function App() {
-
   return (
-   <>
-   <Navbar />
-   </>
-   
-  )
+    <>
+      <Navbar />
+    </>
+  );
 }
 
-export default App
+export default App;
