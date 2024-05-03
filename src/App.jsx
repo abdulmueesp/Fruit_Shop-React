598cd6e63b74cfbf1de8e05b556d5e3371ea4bb7
abdulmueesp <abdulmueesp5@gmail.com>
2024-05-03 12:08:04 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
+import Navbar from "./Components/Navbar"
 
 
 function App() {
-  
 
   return (
-    <>
-     <span className="text-green-500 text-5xl font-bold">Fruit</span>
-    <span className="text-4xl font-light">Shop</span>
-    
-    <div>
-      <span class="text-stone-500 text-4xl">Seoson Specials</span>
-    </div>
-    </>
+   <>
+   <Navbar />
+   </>
    
   )
 }
